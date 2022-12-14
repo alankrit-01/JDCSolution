d62c069d364cda66e1fa33f6039f9da25098213c
0xalankrit <alankritnokhwal@gmail.com>
2022-12-14 12:14:49 +0530

    git init

---
+++ contracts/Supplychain.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;                 
+
+contract Supplychain{
+
+    struct Item{
+        uint supplyChainId;             // Counter for this batch items
+        address RawMaterialSupplierID;  
+        uint timeStamp0;
+    }
+
+}
