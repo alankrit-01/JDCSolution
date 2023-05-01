ec64ce435f94b1a9f2da8830de22b732a5503a43
Babulal <rmwtoken5@richmint.com>
2023-05-01 10:16:27 +0530

    commit 01-05-23

--- scmAdmin/src/pages/SuperAdmin/Company/Company.js
+++ scmAdmin/src/pages/SuperAdmin/Company/Company.js
@@ -213,9 +213,7 @@ const Company = () => {
         <div className="md:ml-32">
           <div className="bg-gray-500 pt-14 pb-28 px-3 md:px-8 h-auto">
             <div className="container mx-auto max-w-full">
-              <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-5">
-                <MainStatusCard />
-              </div>
+                {/* <MainStatusCard /> */}
             </div>
           </div>
           <div className="px-3 md:px-8 h-auto -mt-24">
